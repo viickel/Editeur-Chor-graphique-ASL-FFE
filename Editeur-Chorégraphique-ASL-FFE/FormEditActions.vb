87f33aef7f00a6cea0c9330041bb42185786b0e3
viickel <v.thivolle@gmail.com>
2025-07-19 11:35:32 +0200

    Ajout des catégorie de combat

--- Editeur-Chorégraphique-ASL-FFE/FormEditActions.vb
+++ Editeur-Chorégraphique-ASL-FFE/FormEditActions.vb
@@ -540,8 +540,7 @@ Public Class FormEditActions
         End If
     End Sub
 
+    Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel1.Paint
 
-
-
-
+    End Sub
 End Class
